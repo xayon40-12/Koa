b5908da2a9a25f6197ed3bc63db1e940626c9a82
xayon40-12 <touroux.nathan@gmail.com>
2019-06-10 13:10:01 +0200

    use string literal and add new concepts

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,12 @@ mod test {
 
     #[test]
     fn print_var() {
-        let script = String::from("123.4 4094\n 24 2049 \"aonau89&$\t%oe7\\tuhdu tnoehu lrecu\" 43 \n   098.09");
-        let mut parser = KoaParser::new(script);
-        parser.parse();
+        let script = String::from(r#"
+123.4 {313, "natoh\\eu09348u",{0988,0,"aoecu7a9e","\""}} 4094
+24 2049 "aon\nau89&$    %oe7\tuhdu tnoehu lrecu" 43
+098.09
+"#);
+        let mut parser = KoaParser::new();
+        parser.parse(script);
     }
 }
