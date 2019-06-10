ad4dd11c272d1502555ff73d96f64f6fadda3bda
xayon40-12 <touroux.nathan@gmail.com>
2019-06-10 13:09:09 +0200

    move parsing parameters into ScriptIt struct and remove spaces before and after parsing

--- src/koa_parser.rs
+++ src/koa_parser.rs
@@ -1,13 +1,10 @@
 use std::str::Chars;
 pub mod env;
 pub use env::Env;
-pub use env::types::{Any,Str,Num};
+pub use env::types::{Any,Str,Num,Array};
 
 pub struct KoaParser<'a> {
-    env: Env<'a>,
-    prog: String,
-    line_id: u32,
-    index: usize
+    env: Env<'a>
 }
 
 pub type Res = Result<Box<dyn Any>,ParseErr>;
@@ -17,49 +14,50 @@ pub enum ParseErr {
     Eof
 }
 
-pub struct ScriptIt<'a> {
+pub struct ScriptIt<'a,'b> {
+    env: &'a mut Env<'b>,
     c: char,
     chars: &'a mut Chars<'a>,
-    line_id: &'a mut u32
+    line_id: u32
 }
 
 impl<'a> KoaParser<'a> {
-    pub fn new(prog: String) -> KoaParser<'a>{
-        KoaParser{env: Env::global(), prog, line_id: 1, index: 0}
+    pub fn new() -> KoaParser<'a>{
+        KoaParser{env: Env::global()}
     }
-    pub fn parse(&mut self) {
-        let chars = &mut self.prog.chars();
-        let mut it = ScriptIt {c: chars.next().expect("Parse Error: Empty program. Abort."), chars, line_id: &mut self.line_id};
+    pub fn parse(&mut self, prog: String) {
+        let chars = &mut prog.chars();
+        let mut it = ScriptIt {env: &mut self.env, c: chars.next().expect("Parse Error: Empty program. Abort."), chars, line_id: 1};
         loop {
             let v = Self::parse_next(&mut it);
             match v {
                 Ok(val) => {
                     //TODO
-                    println!("l:{}: ({:?}) {}", *it.line_id, val.get_type(), &val);
+                    println!("l:{}: ({:?}) {}", it.line_id, val.get_type(), &val);
                 },
                 Err(e) => match e {
-                    ParseErr::Parse(msg) => panic!("Parsing Error l:{}: {}", self.line_id, msg),
+                    ParseErr::Parse(msg) => panic!("Parsing Error l:{}: {}", it.line_id, msg),
                     ParseErr::Eof => break
                 }
             }
         }
     }
     fn parse_next(mut it: &mut ScriptIt) -> Res {
-        match it.c {
+        Self::skip_white_space(&mut it);
+        let res = match it.c {
             '0' ..= '9' => Num::parse(&mut it),
             '"' => Str::parse(&mut it),
+            '{' => Array::parse(&mut it),
             '\n' => {
-                *it.line_id += 1;
+                it.line_id += 1;
                 Self::parse_next_char(&mut it);
                 Self::parse_next(&mut it)
-            }
-            ' ' => {
-                Self::skip_white_space(&mut it);
-                Self::parse_next(&mut it)
-            }
+            },
             '\0' => Err(ParseErr::Eof),
-            _ => Err(ParseErr::Parse(String::from("Unrecognised character.")))
-        }
+            _ => Err(ParseErr::Parse(format!("Unrecognised character '{}'.", &it.c)))
+        };
+        Self::skip_white_space(&mut it);
+        res
     }
     fn skip_white_space(mut it: &mut ScriptIt) {
         while it.c == ' ' {
